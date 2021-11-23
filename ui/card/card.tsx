78319734b56c91b90b5ae395aeee5cb17e55b140
Joy <joybarranis@Joys-MacBook-Pro.local>
2021-11-23 11:54:14 -0500

    init

---
+++ ui/card/card.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ExternalLink } from '@teambit/design.ui.external-link';
+import { CopyBox } from '@teambit/documenter.ui.copy-box';
+
+import styles from './card.module.scss';
+
+export type CardProps = {
+  /**
+   * link for the card
+   */
+  link?: string;
+  /**
+   * heading for the card
+   */
+  heading?: string;
+  /**
+   * text for the card
+   */
+  text?: string;
+  /**
+   * command for the copy box component
+   */
+  command?: string;
+};
+
+export function Card(
+  {
+    heading, command, text, link,
+  }: CardProps,
+) {
+  return (
+    <div className={styles.card}>
+      <ExternalLink href={link}>
+        <p className={styles.text}>{text}</p>
+      </ExternalLink>
+      {command ? (
+        <CopyBox className={styles['copy-box']}>{command}</CopyBox>
+      ) : null}
+    </div>
+  );
+}
